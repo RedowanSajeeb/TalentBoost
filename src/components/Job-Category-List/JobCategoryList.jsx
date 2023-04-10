e62fd3a8892ef9cd76e38109123b3136ff3d3d41
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-04-10 11:10:04 +0600

    new-saction-start

--- src/components/Job-Category-List/JobCategoryList.jsx
+++ src/components/Job-Category-List/JobCategoryList.jsx
@@ -9,8 +9,10 @@ const JobCategoryList = () => {
     }, []);
 console.log(list);
     return (
-      <div className="side-container md:mt-32 mt-10">
-        <h1 className="text-center mb-2 text-3xl font-bold">Job Category List</h1>
+      <div className="side-container md:mt-32 mt-10 mb-32">
+        <h1 className="text-center mb-2 text-3xl font-bold">
+          Job Category List
+        </h1>
         <p className="text-center mb-8">
           Explore thousands of job opportunities with all the information you
           need. Its your future
@@ -18,10 +20,14 @@ console.log(list);
         <div className="md:flex  items-center justify-center justify-between mt-8">
           {list.map((categorie, idx) => (
             <div
-              className="flex flex-col items-center justify-center"
+              className="flex flex-col items-center justify-center card-style-short py-2 px-10"
               key={idx}
             >
-              <img className="h-28 mt-8" src={categorie.logo} alt="" />
+              <img
+                className="h-28 mt-8 "
+                src={categorie.logo}
+                alt=""
+              />
               <h1>{categorie.name}</h1>
               <h4>Jobs Available {categorie.jobs_available}</h4>
             </div>
