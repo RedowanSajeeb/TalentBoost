f1058648989e4104872ca53e33256f9356bf3e9f
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-04-11 17:40:20 +0600

    filter-by-add

--- src/components/Applied-Jobs/AppliedJobs.jsx
+++ src/components/Applied-Jobs/AppliedJobs.jsx
@@ -4,6 +4,7 @@ import { getShoppingCart } from "../utilities/fakedb";
 import ShowAppliedJobs from "./ShowAppliedJobs";
 import Banner from "../CommonBanar/Banner";
 import Bannerrr from "../CommonBanar/Bannerrr";
+import { Select, Option } from "@material-tailwind/react";
 
 const AppliedJobs = () => {
   const appliedJobs = useLoaderData();
@@ -20,6 +21,16 @@ const AppliedJobs = () => {
     <div>
       <Bannerrr></Bannerrr>
 
+      <div className="w-72 absolute right-96">
+        <Select className="" label="Filter By">
+          <button className="ms-2 me-3">
+            <Option>remote option</Option>
+          </button>
+          <button className="mt-3">
+            <Option>on-site option</Option>
+          </button>
+        </Select>
+      </div>
       <div className="md:mt-36 mb-10">
         {appliedJobes.map((job, idx) => (
           <ShowAppliedJobs job={job} key={idx}></ShowAppliedJobs>
